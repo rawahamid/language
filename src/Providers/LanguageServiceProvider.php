5a404c28707e29723234af4b3c0d54581de5d2c6
rawahamid <rawahamid4321@gmail.com>
2020-07-04 17:22:31 +0300

    Init Commit

---
+++ src/Providers/LanguageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Targettech\Language\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LanguageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // dd('Provider Work Now');
+    }
+
+    public function register()
+    {
+
+    }
+}
